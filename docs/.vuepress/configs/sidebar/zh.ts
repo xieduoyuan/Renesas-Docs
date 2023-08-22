ec7c74f606a0a083cea57530333446bd3cb12410
myj020329 <mengyingjie@example.com>
2023-08-22 11:36:20 +0800

    docs(object_oriented_module_programming_method_in_ARM_embedded_system): add chapter14

--- docs/.vuepress/configs/sidebar/zh.ts
+++ docs/.vuepress/configs/sidebar/zh.ts
@@ -20,6 +20,7 @@ export const sidebarZh: SidebarConfig = {
         '/zh/DShanMCU_RA6M5/object_oriented_module_programming_method_in_ARM_embedded_system/chapter11.md',
         '/zh/DShanMCU_RA6M5/object_oriented_module_programming_method_in_ARM_embedded_system/chapter12.md',
         '/zh/DShanMCU_RA6M5/object_oriented_module_programming_method_in_ARM_embedded_system/chapter13.md',
+        '/zh/DShanMCU_RA6M5/object_oriented_module_programming_method_in_ARM_embedded_system/chapter14.md',
         '/zh/DShanMCU_RA6M5/object_oriented_module_programming_method_in_ARM_embedded_system/chapter16.md',
       ],
     },
