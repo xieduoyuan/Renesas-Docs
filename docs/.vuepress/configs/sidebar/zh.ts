5c60e45d6b016014df67933fa792d24f9df7483c
Duoyuan <1914335147@qq.com>
2023-10-19 18:24:45 +0800

    docs(freertos): add chapter11

--- docs/.vuepress/configs/sidebar/zh.ts
+++ docs/.vuepress/configs/sidebar/zh.ts
@@ -91,6 +91,7 @@ export const sidebarZh: SidebarConfig = {
         '/zh/DShanMCU_RA6M5/lvgl_port_special_tutorial/README.md',
         '/zh/DShanMCU_RA6M5/lvgl_port_special_tutorial/chapter9.md',
         '/zh/DShanMCU_RA6M5/lvgl_port_special_tutorial/chapter10.md',
+        '/zh/DShanMCU_RA6M5/lvgl_port_special_tutorial/chapter11.md',
       ],
     },
   ],
